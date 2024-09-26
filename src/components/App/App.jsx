131b0cbbd34d3c134d15729533a995904b357879
Eugene-Khonich <ehonich@gmail.com>
2024-09-26 22:35:38 +0300

    interface done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,38 @@
-import "./App.module.css";
-import Description from "../Description/Description";
-import Option from "../Options/Options";
-import Feedback from "../Feedback/Feedback";
+import './App.module.css';
+import { useState } from 'react';
+import Description from '../Description/Description';
+import Option from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
 
+const initialState = { good: 0, neutral: 0, bad: 0 };
 const App = () => {
-  // const [feedbacks, setFeedbacks] = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
+  const [feedbacks, setFeedbacks] = useState(initialState);
+  const updateFeedback = feedbackType => {
+    setFeedbacks({ ...feedbacks, [feedbackType]: feedbacks[feedbackType] + 1 });
+  };
+  const resetFeedback = () => {
+    setFeedbacks(initialState);
+  };
+  const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+  const positiveFeedback = Math.round((feedbacks.good / totalFeedback) * 100);
   return (
     <div>
       <Description />
-      <Option />
-      <Feedback />
+      <Option
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback ? (
+        <Feedback
+          feedbacks={feedbacks}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,15 @@
-import css from "./Feedback.module.css";
+// import css from './Feedback.module.css';
 
-const Feedback = () => {
-  return;
+const Feedback = ({ feedbacks, totalFeedback, positiveFeedback }) => {
+  return (
+    <ul>
+      <li>Good: {feedbacks.good}</li>
+      <li>Neutral: {feedbacks.neutral}</li>
+      <li>Bad: {feedbacks.bad}</li>
+      <li>Total: {totalFeedback}</li>
+      <li>Positive: {positiveFeedback} %</li>
+    </ul>
+  );
 };
 
 export default Feedback;
