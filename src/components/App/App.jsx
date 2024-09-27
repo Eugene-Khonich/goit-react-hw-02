28d3288f9fecb69704811812039bed2a3dc18439
Eugene-Khonich <ehonich@gmail.com>
2024-09-27 22:40:24 +0300

    123

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,37 @@
 import './App.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Description from '../Description/Description';
 import Option from '../Options/Options';
 import Feedback from '../Feedback/Feedback';
 import Notification from '../Notification/Notification';
 
 const initialState = { good: 0, neutral: 0, bad: 0 };
+
 const App = () => {
-  const [feedbacks, setFeedbacks] = useState(initialState);
+  const [feedbacks, setFeedbacks] = useState(() => {
+    const storageFeedback = localStorage.getItem('feedbackValue');
+    if (storageFeedback !== null) {
+      return JSON.parse(storageFeedback);
+    }
+    return initialState;
+  });
+
   const updateFeedback = feedbackType => {
     setFeedbacks({ ...feedbacks, [feedbackType]: feedbacks[feedbackType] + 1 });
   };
+
   const resetFeedback = () => {
     setFeedbacks(initialState);
   };
+
   const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+
   const positiveFeedback = Math.round((feedbacks.good / totalFeedback) * 100);
+
+  useEffect(() => {
+    localStorage.setItem('feedbackValue', JSON.stringify(feedbacks));
+  }, [feedbacks]);
+
   return (
     <div>
       <Description />
